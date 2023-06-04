8f0ab7b7510587887f71a969225d92b1abaf5362
Alisson-Py <alissilva1231@hotmail.com>
2023-06-04 13:32:01 -0300

    feat: create suplier endpoints

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -3,6 +3,7 @@ import { Router, response } from "express";
 import ClientsControllers from "./controllers/ClientsControllers";
 import EnterpriseControllers from "./controllers/EnterpriseControllers";
 import ProductsControllers from "./controllers/ProductsControllers";
+import SupliersControllers from "./controllers/SupliersControllers";
 
 
 
@@ -10,6 +11,7 @@ const routes = Router();
 const clients = new ClientsControllers();
 const enterprise = new EnterpriseControllers();
 const products = new ProductsControllers();
+const supliers = new SupliersControllers();3
 
 routes.get("/", (req, res) => {return res.json(["ok"])});
 
@@ -30,5 +32,11 @@ routes.get("/product/:id", products.show);
 routes.post("/product/create", products.store);
 routes.put("/product/update/:id", products.update);
 routes.delete("/product/delete/:id", products.delete);
+
+routes.get("/supliers", supliers.index);
+routes.get("/suplier/:id", supliers.show);
+routes.post("/suplier/create", supliers.store);
+routes.put("/suplier/update/:id", supliers.update);
+routes.delete("/suplier/delete/:id", supliers.delete);
 
 export default routes;
