fa5235791b0661aa04dbe395f81d98373cc2d5a2
Alisson-Py <alissilva1231@hotmail.com>
2023-07-07 17:50:21 -0300

    feat: create update function rule and limits what the server search into db

--- src/app/controllers/SupliersControllers.ts
+++ src/app/controllers/SupliersControllers.ts
@@ -5,14 +5,17 @@ class SupliersControllers {
     async index(req: Req, res: Res): Promise<Res<any>> {
         try {
             const supliers = await database.supliers.findMany({
-                include:{
-                    _count:true,
+                select: {
+                    id: true,
+                    name: true,
+                    category: true,
                     product: {
-                        select: {
-                            id: true
-                        }
+                       select: {
+                        id: true
+                       }
                     }
-                }
+                },
+                
             });
 
             return res.json({data: supliers});
@@ -22,8 +25,18 @@ class SupliersControllers {
         }
     };
     async show(req: Req, res: Res): Promise<Res<any>> {
+        const {id} = req.params;
         try {
-            return res.json({ok: true});
+            const suplier = await database.supliers.findUnique({
+                where: {id},
+                select: {
+                    id: true,
+                    name: true,
+                    cnpj: true,
+                }
+            });
+            if (!suplier) return res.status(404).json({err: "suplier not found"});
+            return res.json({data: suplier});
         } catch (err) {
             console.log(err);
             return res.status(500).json({ok: false});
@@ -36,25 +49,38 @@ class SupliersControllers {
             cnpj
         } = req.body;
         try {
-            await database.supliers.create({
+            const suplier = await database.supliers.create({
                 data: {
                     name,
                     category,
                     cnpj,
                 }
             });
-            return res.json({ok: true});
+            return res.json({created: true, id: suplier.id});
         } catch (err) {
             console.log(err);
-            return res.status(500).json({ok: false});
+            return res.status(500).json({created: false});
         }
     };
     async update(req: Req, res: Res): Promise<Res<any>> {
+        const { name, category } = req.body;
+        const { id } = req.params;
         try {
-            return res.json({ok: true});
+            const suplier = await database.supliers.findUnique({
+                where: { id }
+            });
+            if (!suplier) return res.status(404).json({err: "suplier not found"});
+            await database.supliers.update({
+                where: { id },
+                data: {
+                    category: category? category: suplier.category,
+                    name: name? name: suplier.name
+                }
+            });
+            return res.json({updated: true});
         } catch (err) {
             console.log(err);
-            return res.status(500).json({ok: false});
+            return res.status(500).json({udpated: false});
         }
     };
     async delete(req: Req, res: Res): Promise<Res<any>> {
