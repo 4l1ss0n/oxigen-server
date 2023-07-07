f769e58d3e588a67eee8c195be2696e417e74708
Alisson-Py <alissilva1231@hotmail.com>
2023-07-07 17:39:17 -0300

    feat: create update function rule and limits what the server search into db

--- src/app/controllers/ProductsControllers.ts
+++ src/app/controllers/ProductsControllers.ts
@@ -3,18 +3,15 @@ import { database } from "../../database/src";
 
 class ProductsControllers {
     async index(req: Req, res: Res): Promise<Res<any>> {
+        const profit = process.env.PROFIT_PERCENTAGE;
         try {
             const products = await database.products.findMany({
-                include: {
-                    _count: true,
-                    supliers: {
-                        select: {
-                            id: true
-                        }
-                    }
+                select: {
+                    id: true,
+                    name: true,
+                    cost: true
                 }
             });
-
             return res.json({data: products});
         } catch (err) {
             console.log(err);
@@ -22,8 +19,23 @@ class ProductsControllers {
         }
     };
     async show(req: Req, res: Res): Promise<Res<any>> {
+        const {id} = req.params;
         try {
-            return res.json({ok: true});
+            const product = await database.products.findUnique({
+                where: {id},
+                select: {
+                    id: true,
+                    name: true,
+                    description: true,
+                    quantity: true,
+                    cost: true,
+                    color: true,
+                    components: true
+                }
+            });
+
+            if (!product) return res.status(404).json({err: "product not found"});
+            return res.json({data: product});
         } catch (err) {
             console.log(err);
             return res.status(500).json({ok: false});
@@ -57,15 +69,40 @@ class ProductsControllers {
             await database.productSupiers.createMany({
                 data: supliersProducts
             })
-            return res.json({ok: true});
+            return res.json({
+                created: true,
+                id: product.id
+            });
         } catch (err) {
             console.log(err);
-            return res.status(500).json({ok: false});
+            return res.status(500).json({created: false});
         }
     };
     async update(req: Req, res: Res): Promise<Res<any>> {
+        const {
+            name,
+            description,
+            quantity,
+            cost,
+            color,
+        } = req.body;
+        const {id} = req.params;
         try {
-            return res.json({ok: true});
+            const product = await database.products.findUnique({
+                where: {id}
+            });
+            if (!product) return res.status(404).json({err: "product not found"});
+            await database.products.update({
+                where: {id},
+                data: {
+                    name: name? name: product.name,
+                    description: description? description: product.description,
+                    quantity: quantity? quantity: product.quantity,
+                    color: color? color: product.color,
+                    cost: cost? cost: product.cost
+                }
+            })
+            return res.json({updated: true});
         } catch (err) {
             console.log(err);
             return res.status(500).json({ok: false});
