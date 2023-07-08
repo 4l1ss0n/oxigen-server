2f84b81a90d6193a883f2329e4696cff5c94a5f6
Alisson-Py <alissilva1231@hotmail.com>
2023-07-08 16:37:43 -0300

    fix: change word meaning

--- src/app/middleware/tokenGen.ts
+++ src/app/middleware/tokenGen.ts
@@ -2,8 +2,8 @@ import jwt from "jsonwebtoken";
 
 const secret = process.env.SECRET_KEY_JWT || "Alisson";
 
-export default (id: {}, temp: string): string => {
+export default (id: any, time: string): string => {
     return jwt.sign(id, secret, {
-        expiresIn: temp
+        expiresIn: time
     })
 };
